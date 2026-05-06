99b12a451ab72142a77153eaee41eb8ae45b1623
Wallostonite <abiodun.ayobalies@gmail.com>
2026-05-05 21:39:06 -0400

    updated version tested config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 # Load all variables from .env into the environment
 load_dotenv()
 
-INDUSTRY       = os.getenv("INDUSTRY",       "bootcamp_data")
+INDUSTRY       = os.getenv("INDUSTRY",       "healthcare")
 LEARNER_SCHEMA = os.getenv("LEARNER_SCHEMA", "learner_05")
 
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parent
